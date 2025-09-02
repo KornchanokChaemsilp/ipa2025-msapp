bde13e1befb55415bb68beb95a80c90611dc0d6a
KornchanokChaemsilp <66070001@kmitl.ac.th>
2025-09-02 18:48:49 +0700

    stage 3

---
+++ scheduler/database.py
@@ -0,0 +1,19 @@
+import os
+
+from pymongo import MongoClient
+
+def get_router_info():
+    mongo_uri = os.environ.get("MONGO_URI")
+    dbname = os.environ.get("DB_NAME")
+
+    client = MongoClient(mongo_uri)
+    db = client[dbname]
+    routers = db["routers"]
+
+    router_data = routers.find()
+    # for data in router_data:
+    #     print(data)
+    return router_data
+
+if __name__ == "__main__":
+    get_router_info()
---
+++ scheduler/producer.py
@@ -0,0 +1,31 @@
+import pika
+import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+def produce(host, body):
+    rabbitmq_username = os.getenv("RABBITMQ_DEFAULT_USER")
+    rabbitmq_password = os.getenv("RABBITMQ_DEFAULT_PASS")
+
+    print(rabbitmq_username, rabbitmq_password)
+
+    credentials = pika.PlainCredentials(rabbitmq_username, rabbitmq_password)
+
+    parameters = pika.ConnectionParameters(
+        host=host,
+        credentials=credentials
+    )
+
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange="jobs", exchange_type="direct")
+    channel.queue_declare(queue="router_jobs")
+    channel.queue_bind(queue="router_jobs", exchange="jobs", routing_key="check_interfaces")
+
+    channel.basic_publish(exchange="jobs", routing_key="check_interfaces", body=body)
+
+    channel.close()
+
+if __name__ == "__main__":
+    produce("localhost", "192.168.1.44")
